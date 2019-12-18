4dbe83d9fb8961581bc4c0005c1272c89e552468
lakshan28 <lakshansampath28@gmail.com>
2019-12-18 16:42:32 +0530

    first commt

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useSpring, animated as a } from "react-spring";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [flipped, set] = useState(false);
+  const { transform, opacity } = useSpring({
+    opacity: flipped ? 1 : 0,
+    transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg)`,
+    config: { mass: 5, tension: 500, friction: 80 }
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div onClick={() => set(state => !state)}>
+      <a.div
+        class="c back"
+        style={{ opacity: opacity.interpolate(o => 1 - o), transform }}
+      >
+        Login
+      </a.div>
+      <a.div
+        class="c front"
+        style={{
+          opacity,
+          transform: transform.interpolate(t => `${t} rotateX(180deg)`)
+        }}
+      >
+        Register
+      </a.div>
     </div>
   );
 }
-
-export default App;
